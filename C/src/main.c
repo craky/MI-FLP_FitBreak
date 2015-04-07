a54387529fbeeb6aac88aa221901e4be65ffdc9e
Vojtech Krakora <v.krakora@gmail.com>
2015-04-07 18:28:18 +0200

    Commented header

--- C/src/main.c
+++ C/src/main.c
@@ -1,3 +1,9 @@
+/* FitBreak */
+/* @author: krakovoj@fit.cvut.cz */
+/* @version: 1.0 */
+/* (c) LS 2015 */
+/* MI - FLP */
+/********************************************/
 int main(void){
 	return 0;
 }
