40a9207c11e70caf4a964d5819178df3e5aa5753
Vojtech Krakora <v.krakora@gmail.com>
2015-05-03 15:55:42 +0200

    Bellman-Ford algorithm with no loops

--- LISP/main.lisp
+++ LISP/main.lisp
@@ -68,11 +68,52 @@
 										(setf (aref *adjacency_matrix* m n) d)
 										(load_streets (- remain_str 1))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Check the distance
+;; If shorter distance was found, set it.
+(defun check_distance (y k)
+	(if (< (+ (aref *distance* y) (aref *adjacency_matrix* y k)) (aref *distance* k))
+		(progn (setf (aref *distance* k) (+ (aref *distance* y) (aref *adjacency_matrix* y k)))
+				 (setf (aref *predecessor* k) y))
+		ok))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; k nodes
+;; It is simulation of third loop in three nested loops
+(defun k_nodes (y k)
+	(if (> (aref *adjacency_matrix* y k) 0)
+		(check_distance y k)
+		ok))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; j nodes
+;; It is simulation of second loop in three nested loops
+(defun j_nodes (y)
+	(if (eq y 0)
+		ok
+		(progn (k_nodes y *n*)
+			    (j_nodes (- y 1)))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; through all nodes
+;; will do cycle through all nodes
+;; It is simulation of first loop in three nested loops
+(defun through_all_nodes (x)
+	(if (eq x 0)
+		ok
+		(progn (j_nodes *n*)
+				 (through_all_nodes (- x 1)))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;Bellman - Ford algorithm
-(defun bellman-ford() )
+(defun bellman-ford()
+	;; set distance of first node 0
+	(setf (aref *distance* 0) 0)
+	(through_all_nodes *n*)
+	ok
+	)
 
+(setf *n* 10)
 (print (assert_ (load_streets (load_num_of_streets)) ok "Test: Number of streets is not same!"))
-(print (assert_ (aref *adjacency_matrix* 3 2) 5 "Test: Adjacency matrix on [3:2] is not 5"))
+(print (assert_ (aref *adjacency_matrix* 3 2) 5 "Test: Adjacency matrix on [3:2] is not 5."))
+(print (assert_ (through_all_nodes *n*) ok "Test: through_all_nodes does not return ok."))
+(print (assert_ (j_nodes *n*) ok "Test: j_nodes does not return ok."))
+(print (assert_ (bellman-ford) ok "Test: bellman-ford does not return ok."))
 ;(print (load_num_of_node))
 ;(print (load_num_of_streets))
 
